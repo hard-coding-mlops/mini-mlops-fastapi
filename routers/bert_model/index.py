991b3d857b7329414107e4eb54d4dc737e4e3e4d
Monte <monte6198@gmail.com>
2023-11-29 09:54:30 +0900

    comment: 에러 발생 부분 주석처리

--- routers/bert_model/index.py
+++ routers/bert_model/index.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException, status, Query
-from .KoBert import bert_learn
+# from .KoBert import bert_learn
 
 router = APIRouter()
 
 @router.get("/learn", status_code = status.HTTP_200_OK)
 async def learn():
-    bert_learn()
+    # bert_learn()
     return {
         "status": "success",
         "message": "[Mini MLOps] GET /model/learn 완료되었습니다.",
