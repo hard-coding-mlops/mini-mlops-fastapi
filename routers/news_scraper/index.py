73d473a2886a54ef0687ab20bc1c50df348fdd4a
taehwan01 <monte6198@gmail.com>
2023-12-21 17:34:46 +0900

    feat: 수집 진행률 추가

--- routers/news_scraper/index.py
+++ routers/news_scraper/index.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, status
 import traceback
 from datetime import datetime
+import json
+from fastapi.responses import StreamingResponse
 
 from models.news_article import NewsArticle
 from models.scraped_order import ScrapedOrder
@@ -39,16 +41,26 @@ async def read_all_news_articles(db: db_dependency):
 #         traceback.print_exc()
 #         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/scrape", status_code=status.HTTP_200_OK)
-async def scrape_news_articles(db: db_dependency):
+def scrape_news_articles(db: db_dependency):
     try:
         print("\n\033[36m[Mini MLOps] \033[37m", end=' ')
 
+        data = {
+            'kind': "수집 시작",
+            'progress': 5,
+        }
+        yield f"data: {json.dumps(data)}\n\n"
+        
         news_scraper = NewsScraper()
         results = news_scraper.run()
         print("\n\033[36m[Mini MLOps] \033[32m뉴스 스크래핑을 마치고 데이터베이스에 저장합니다.")
         print("\033[36m[Mini MLOps] \033[33m이 작업은 꽤 걸립니다.\n")
         
+        data = {
+            'kind': "수집 데이터베이스 저장",
+            'progress': 15,
+        }
+        yield f"data: {json.dumps(data)}\n\n"
         # last_scraped_order = db.query(ScrapedOrder).order_by(ScrapedOrder.id.desc()).limit(1).first()
         # current_order_no = last_scraped_order._no + 1 if last_scraped_order else 1
         results_last_index = len(results) - 1
@@ -62,12 +74,31 @@ async def scrape_news_articles(db: db_dependency):
         db.add(scraped_order)
         db.commit()
         db.refresh(scraped_order)
+        
+        total_articles = sum(len(sublist) for sublist in results)
+        current_progress = 15
     
         for articles in results:
             for article in articles:
+                # 로딩 값 표시
                 save_news_article(db, article, scraped_order.id)
-
-        return {"status": "success", "message": "[Mini MLOps] 뉴스 스크래핑을 완료했습니다.\n"}
+                current_progress += (1 / total_articles) * 84
+                data = {
+                    'kind': "수집 데이터베이스 저장",
+                    'progress': current_progress,  # 15 ~ 99
+                }
+                yield f"data: {json.dumps(data)}\n\n"
+        
+        data = {
+            'kind': "수집 완료",
+            'progress': 100,
+        }
+        yield f"data: {json.dumps(data)}\n\n"
+        print("\033[36m[Mini MLOps] \033[32m뉴스 스크래핑을 완료했습니다.")
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/scrape", status_code=status.HTTP_200_OK)
+def scrape_news_articles_sse(db: db_dependency):
+    return StreamingResponse(scrape_news_articles(db), media_type="text/event-stream")
