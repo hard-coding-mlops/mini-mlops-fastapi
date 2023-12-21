deef1dea9805030b1b48a0e8214c2bc04a1d1635
taehwan01 <monte6198@gmail.com>
2023-12-21 17:38:31 +0900

    feat: 정제 진행률 추가

--- routers/preprocessor/index.py
+++ routers/preprocessor/index.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, status
 import traceback
 import re
-# from kobert_tokenizer import KoBERTTokenizer
+from fastapi.responses import StreamingResponse
+import json
 
 from models.news_article import NewsArticle
 from models.preprocessed_article import PreprocessedArticle
@@ -13,8 +14,25 @@ from routers import news_scraper
 router = APIRouter()
 # tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1', sp_model_kwargs={'nbest_size': -1, 'alpha': 0.6, 'enable_sampling': True})
 
-@router.get("/preprocess", status_code = status.HTTP_200_OK)
+
 async def preprocess_articles(db: db_dependency):
+    async def calculate_progress(total, current, start_percentage, end_percentage):
+        return start_percentage + (current / total * (end_percentage - start_percentage))
+
+    async def send_progress(kind, current, total, start_percentage, end_percentage):
+        progress = await calculate_progress(total, current, start_percentage, end_percentage)
+        data = {
+            'kind': kind,
+            'progress': progress,
+        }
+        yield f"data: {json.dumps(data)}\n\n"
+
+    data = {
+        'kind': "정제 시작",
+        'progress': 1,
+    }
+    yield f"data: {json.dumps(data)}\n\n"
+    
     last_scraped_order = (
         db.query(NewsArticle.scraped_order_no)
         .order_by(NewsArticle.scraped_order_no.desc())
@@ -32,12 +50,90 @@ async def preprocess_articles(db: db_dependency):
     non_duplicated_contents = set()
     non_duplicated_articles = []
     
+    total_articles = len(last_scraped_news_articles)
+    for index, article in enumerate(last_scraped_news_articles):
+        async for progress_data in send_progress("중복 제거", index, total_articles, 5, 25):
+            yield progress_data
+        
+        if article.content not in non_duplicated_contents:
+            non_duplicated_contents.add(article.content)
+            non_duplicated_articles.append(article)
+    
+    total_non_duplicated_articles = len(non_duplicated_articles)
+    for index, article in enumerate(non_duplicated_articles):
+        async for progress_data in send_progress("한글 이외 제거", index, total_non_duplicated_articles, 25, 75):
+            yield progress_data
+        
+        preprocessed_article = PreprocessedArticle()
+        article.title = re.sub('[^가-힣 ]', '', article.title).strip()
+        article.content = re.sub('[^가-힣 ]', '', article.content).strip()
+        length_of_content = len(article.content)
+        
+        if length_of_content > 10:
+            formatted_text = article.title + article.content
+            preprocessed_article.category_no = category_label[article.category]
+            preprocessed_article.formatted_text = formatted_text
+            preprocessed_article.original_article_id = article.id
+            db.add(preprocessed_article)
+            db.commit()
+            db.refresh(preprocessed_article)
+    
+    data = {
+        'kind': "정제 완료",
+        'progress': 100,
+    }
+    yield f"data: {json.dumps(data)}\n\n"
+    
+    print(f"\n\033[36m[Mini MLOps] \033[32m데이터 정제가 완료되었습니다.")
+
+@router.get("/preprocess", status_code=status.HTTP_200_OK)
+async def preprocess_articles_sse(db: db_dependency):
+    return StreamingResponse(preprocess_articles(db), media_type="text/event-stream")
+
+
+
+
+# @router.get("/scrape-and-preprocess", status_code = status.HTTP_200_OK)
+def scrape_and_preprocess_articles(db: db_dependency):
+    # news_scraper.index.scrape_news_articles(db)
+    
+    data = {
+        'kind': "정제 시작",
+        'progress': 51,
+    }
+    last_scraped_order = (
+        db.query(NewsArticle.scraped_order_no)
+        .order_by(NewsArticle.scraped_order_no.desc())
+        .limit(1)
+        .first()
+    )[0]
+    last_scraped_news_articles = (
+        db.query(NewsArticle)
+        .filter(NewsArticle.scraped_order_no == last_scraped_order)
+        .all()
+    )
+    
+    print(f"\n\033[36m[Mini MLOps] \033[32m데이터 정제를 시작합니다.")
+    
+    non_duplicated_contents = set()
+    non_duplicated_articles = []
+    
+    data = {
+        'kind': "중복 제거",
+        'progress': 55,
+    }
+    yield f"data: {json.dumps(data)}\n\n"
     # 중복 제거
     for article in last_scraped_news_articles:
         if article.content not in non_duplicated_contents:
             non_duplicated_contents.add(article.content)
             non_duplicated_articles.append(article)
     
+    data = {
+        'kind': "한글 이외 제거",
+        'progress': 60,
+    }
+    yield f"data: {json.dumps(data)}\n\n"
     # 한글 이외 단어들 제거
     preprocessed_articles_length = 0
     for article in non_duplicated_articles:
@@ -45,6 +141,13 @@ async def preprocess_articles(db: db_dependency):
         article.title = re.sub('[^가-힣 ]', '', article.title).strip()
         article.content = re.sub('[^가-힣 ]', '', article.content).strip()
         length_of_content = len(article.content)
+        
+        # 로딩 값 표시 61 ~ 99
+        data = {
+            'kind': "정제 데이터베이스 저장",
+            'progress': 61 + int((length_of_content / 100) * 38),
+        }
+        yield f"data: {json.dumps(data)}\n\n"
         
         if length_of_content > 10:
             preprocessed_articles_length += 1
@@ -58,64 +161,13 @@ async def preprocess_articles(db: db_dependency):
     
     print(f"\n\033[36m[Mini MLOps] \033[32m데이터 정제가 완료되었습니다.")
         
-    return {
-        "status": "success",
-        "message": "[Mini MLOps] 데이터 정제가 완료되었습니다.",
+    data = {
+        'kind': "정제 완료",
+        'progress': 100,
         "length": preprocessed_articles_length,
     }
+    yield f"data: {json.dumps(data)}\n\n"
 
-
-@router.get("/scrape-and-preprocess", status_code = status.HTTP_200_OK)
-async def qwefpreprocess_articles(db: db_dependency):
-    await news_scraper.index.scrape_news_articles(db)
-    
-    last_scraped_order = (
-        db.query(NewsArticle.scraped_order_no)
-        .order_by(NewsArticle.scraped_order_no.desc())
-        .limit(1)
-        .first()
-    )[0]
-    last_scraped_news_articles = (
-        db.query(NewsArticle)
-        .filter(NewsArticle.scraped_order_no == last_scraped_order)
-        .all()
-    )
-    
-    print(f"\n\033[36m[Mini MLOps] \033[32m데이터 정제를 시작합니다.")
-    
-    non_duplicated_contents = set()
-    non_duplicated_articles = []
-    
-    # 중복 제거
-    for article in last_scraped_news_articles:
-        if article.content not in non_duplicated_contents:
-            non_duplicated_contents.add(article.content)
-            non_duplicated_articles.append(article)
-    
-    # 한글 이외 단어들 제거
-    preprocessed_articles_length = 0
-    for article in non_duplicated_articles:
-        preprocessed_article = PreprocessedArticle()
-        article.title = re.sub('[^가-힣 ]', '', article.title).strip()
-        article.content = re.sub('[^가-힣 ]', '', article.content).strip()
-        length_of_content = len(article.content)
-        
-        if length_of_content > 10:
-            preprocessed_articles_length += 1
-            text = article.title + article.content
-            preprocessed_article.category_no = category_label[article.category]
-            preprocessed_article.formatted_text = formatted_text
-            preprocessed_article.original_article_id = article.id
-            preprocessed_article.original_article_id = article.id
-            
-            db.add(preprocessed_article)
-            db.commit()
-            db.refresh(preprocessed_article)
-    
-    print(f"\n\033[36m[Mini MLOps] \033[32m데이터 정제가 완료되었습니다.")
-        
-    return {
-        "status": "success",
-        "message": "[Mini MLOps] 데이터 정제가 완료되었습니다.",
-        "length": preprocessed_articles_length,
-    }
+# @router.get("/scrape-and-preprocess", status_code = status.HTTP_200_OK)
+# def scrape_and_preprocess_articles_sse(db: db_dependency):
+#     return StreamingResponse(scrape_and_preprocess_articles(db), media_type="text/event-stream")
